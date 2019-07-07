5fced2c1bcc392cd994b46f07a110140a21eb754
techsithgit <hemilp@tuplecorp.com>
2019-07-07 09:19:41 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Item from "./components/item";
+
+const initList = [
+  { name: "tomato", calorie: 20 },
+  { name: "rice", calorie: 30 },
+  { name: "candy", calorie: 100 }
+];
 
 function App() {
+  const [list, setList] = useState(initList);
+
+  function removeItemHandle(e) {
+    console.dir(e.target.name);
+
+    const filteredList = list.filter(v => v.name !== e.target.name);
+    setList(filteredList);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Grocery List</h2>
+        {list.map((v, k) => {
+          return (
+            <Item
+              key={`${k}${v.name}${v.calorie}`}
+              item={v}
+              onClick={removeItemHandle}
+            />
+          );
+        })}
       </header>
     </div>
   );
